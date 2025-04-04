f09e35202c420a3d665fa83ee026ce1e92c0ba52
harshithndbs <20061509@mydbs.ie>
2025-04-04 14:31:41 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,56 +7,33 @@ data "aws_vpc" "main" {
   default = true
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  
-  tags = {
-    Name = "main-igw"
+# Fetch default subnets in the VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
   }
 }
 
-# Public Subnets
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  
-  tags = {
-    Name = "public-subnet-1"
+# Fetch existing Internet Gateway if one exists for the default VPC
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [data.aws_vpc.main.id]
   }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+# Public Route Table (using existing or creating a new one)
+resource "aws_route_table" "public_rt" {
+  vpc_id = data.aws_vpc.main.id
   
-  tags = {
-    Name = "public-subnet-2"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = data.aws_internet_gateway.default.id
   }
-}
-
-# Private Subnets
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
   
   tags = {
-    Name = "private-subnet-1"
-  }
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-  
-  tags = {
-    Name = "private-subnet-2"
+    Name = "public-route-table"
   }
 }
 
@@ -69,33 +46,19 @@ resource "aws_eip" "nat_eip" {
   }
 }
 
-# NAT Gateway
+# NAT Gateway (using one of the default subnets)
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_1.id
+  subnet_id     = data.aws_subnets.default.ids[0]
   
   tags = {
     Name = "main-nat-gw"
   }
 }
 
-# Public Route Table
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-  
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  
-  tags = {
-    Name = "public-route-table"
-  }
-}
-
 # Private Route Table
 resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = data.aws_vpc.main.id
   
   route {
     cidr_block     = "0.0.0.0/0"
@@ -107,32 +70,11 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-# Route Table Associations
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
 # Security Group
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Allow web and SSH traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.main.id
   
   # Allow SSH
   ingress {
@@ -178,7 +120,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0440d3b780d96b29d" # Amazon Linux 2023 AMI
   instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public_subnet_1.id
+  subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name               = "eventify-key" # Make sure to create this key pair
   
